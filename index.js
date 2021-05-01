3d8fcd233a773d670a0afa683bf3f027d9482970
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-05-01 00:07:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-function totalBatteries(){
-let  totalBatteries=batteryBatches.reduce( (total, element) => element  + total, 0);
- const number=totalBatteries;
- return number;
-}
-totalBatteries();
+let totalBatteries = batteryBatches.reduce(function(total, element){
+        return element += total}, 0);
